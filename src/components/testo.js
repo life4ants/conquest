120365e65d608a1221383251f24440870301f8c3
Andy Buck <brotherandy@live.com>
2017-03-28 11:21:38 -0400

    remove unused files

--- src/components/testo.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-var Testo = <svg version='1.1' height='25' width='25' viewbox='0 0 26.5 28.5'><path style='fill:#00FFFF; stroke:black; stroke-width:1.5; stroke-linejoin:round'd='m0.5,1 12,5.5 12,-5.5 -5.5,12 5.5,12 -12,-5.5 -12,5.5 5.5,-12 z'/></svg>;
-
-export default Testo;
